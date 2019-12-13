7f1eb8aa1029de18f4f7c7501d434c3630b7535e
Milad Moafi <milixov@gmail.com>
2019-12-13 15:28:25 +0330

    add title layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import Card from "./components/Card";
+import boatImage from "./assets/img/boat/boat-image.png";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Card />
+      <Card
+        image={boatImage}
+        title="Jeannau Sun Odyssey 45.2"
+        year="2011"
+        location="Split, Croatia"
+      />
     </div>
   );
 }
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,20 +1,36 @@
 import React from "react";
+import heartIcon from "../../assets/icon/heart/heart.svg";
 
 import "./style.css";
 
 function Card(props) {
+  const { image, title, year, location } = props;
   return (
     <div className="wrapper">
-      <div className="image">image</div>
-      <div className="title">title</div>
-      <div className="view">view</div>
-      <div className="length">length</div>
-      <div className="cabin">cabin</div>
-      <div className="guest">guest</div>
-      <div className="review">review</div>
-      <div className="desc">desc</div>
-      <div className="price">price</div>
-      <div className="booking">booking</div>
+      <div className="imageWrapper">
+        <img className="img" src={image} alt="Boat Image" />
+        <div className="recommend">Zizoo recommended</div>
+      </div>
+      <div className="titleWrapper">
+        <div className="titleContainer">
+          <div className="titleYear">
+            <span className="title">{title}</span>
+            <span className="year">{year}</span>
+          </div>
+          <span className="location">{location}</span>
+        </div>
+        <div className="favorite">
+          <img src={heartIcon} alt="Favorite" />
+        </div>
+      </div>
+      <div className="viewWrapper">view</div>
+      <div className="lengthWrapper">length</div>
+      <div className="cabinWrapper">cabin</div>
+      <div className="guestWrapper">guest</div>
+      <div className="reviewWrapper">review</div>
+      <div className="descWrapper">desc</div>
+      <div className="priceWrapper">price</div>
+      <div className="bookingWrapper">booking</div>
     </div>
   );
 }
